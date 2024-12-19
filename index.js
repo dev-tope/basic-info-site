cf4ec2404ffd224fef5062d281b377d57158324e
dev-tope <charlestemitope@gmail.com>
2024-12-20 00:45:20 +0100

    Server and routing rewritten in Express

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
-import http, { createServer } from 'http';
+import express from "express";
 import fs from 'fs/promises';
 import url from 'url';
 import path from 'path';
 
-const PORT = process.env.PORT;
+const app = express()
+
+const PORT = process.env.PORT || 8080;
 
 // get filename to get the dirname
 const __filename = url.fileURLToPath(import.meta.url);
-
-
 //get dirname
 const __dirname = path.dirname(__filename);
 
 //create async server, with the ususal try catch block
-const server = createServer(async (req, res) => {
-  try {
-    if(req.method === "GET") {
-      //specify what file to look for based on url path
-      let filepath;
-      if(req.url === '/') {
-        filepath = path.join(__dirname, 'public', 'index.html')
-      } else if (req.url === '/about') {
-        filepath = path.join(__dirname, 'public', 'about.html')
-      } else if (req.url === '/contact-me') {
-        filepath = path.join(__dirname, 'public', 'contact-me.html')
-      } else {
-        filepath = path.join(__dirname, 'public', '404.html')
-      }
-      //then read and write the file as response to the client
-      const data = await fs.readFile(filepath)
-      res.writeHead(200, {'Content-Type' : 'text/html'});
-      res.write(data);
-      res.end()
-
-    } else {
-      throw new Error('Method not allowed')
-    }
-  } catch (error) {
-    res.writeHead(500, {'Content-Type' : 'text/plain'})
-    res.end('Server Error')
+app.get("/", (req, res) => {
+  const options = {
+    root: path.join(__dirname, 'public')
   }
-});
+  res.sendFile('/index.html', options, (err) => {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log('Sent')
+    }
+  })
+})
 
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
+app.get("/about", (req, res) => {
+  const options = {
+    root: path.join(__dirname, 'public')
+  }
+  res.sendFile('/about.html', options, (err) => {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log('Sent')
+    }
+  })
+})
+
+app.get("/contact-me", (req, res) => {
+  const options = {
+    root: path.join(__dirname, 'public')
+  }
+  res.sendFile('/contact-me.html', options, (err) => {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log('Sent')
+    }
+  })
+})
+
+app.get("*", (req, res) => {
+  const options = {
+    root : path.join(__dirname, 'public')
+  }
+  res.status(404).sendFile('/404.html', options, (err) => {
+    if (err) {
+      console.log (err)
+    } else {
+      console.log('Sent')
+    }
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Express App listening on ${PORT}`)
 })
